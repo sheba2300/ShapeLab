0fa6e6a7f1224792f959d77146f78bab6818fb69
sheba sow <ssow8171@bths.edu>
2018-10-02 09:43:48 -0400

    circle class done

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,18 @@
-public class Circle {
+public class Circle extends Shape{
+    private int radius;
+
+    public Circle (int radius)
+    {
+        super("Circle");
+        this.radius = radius;
+    }
+    public double getArea()
+    {
+        return (Math.PI * Math.sqrt(radius));
+    }
+
+    public double getPerimeter()
+    {
+        return ((radius*2) *Math.PI);
+    }
 }
