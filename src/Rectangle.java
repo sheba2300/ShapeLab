66bbf6d983c0658b91742004636cee1323b4009b
Nazmus <nsakib6685@bths.edu>
2018-10-02 10:12:39 -0400

    Update NS 10/2/18 End of Class

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -22,5 +22,13 @@ public class Rectangle extends Shape
         return ( 2*(side1) + 2*(side2));
     }
 
+    public Rectangle(double side1, double side2,String type)
+    {
+        super(type);
+        this.side1 = side1;
+        this.side2 = side2;
+
+    }
+
 
 }
