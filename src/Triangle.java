08bf948d42e7066f57fe741619c19a9d6ced4b24
BT_1N3_24 <jlin7500@bths.edu>
2018-10-02 09:43:07 -0400

    almsot done???

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,19 @@
-public class Triangle {
-}
+public class Triangle extends Shape
+    {
+        private int side;
+        private int height;
+        public Triangle(int side, int height)
+        {
+            super("Triangle");
+            this.side = side;
+            this.height = height;
+        }
+        public double getArea()
+        {
+            return .5*(side * height);
+        }
+        public double getPerimeter()
+        {
+            return side * 3;
+        }
+    }
