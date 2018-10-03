85e4a9d71c04e1b779545ab67d0c5c8076a07759
Jonathan <jlin7500@bths.edu>
2018-10-02 20:35:20 -0400

    Think its done

--- src/ITriangle.java
+++ src/ITriangle.java
@@ -2,14 +2,20 @@ public class ITriangle extends Shape
 {
     private int base;
     private int height;
-    public ITriangle(int base, int height)
+    private int side;
+    public ITriangle(int base, int height, int side)
     {
         super("ITriangle");
         this.base = base;
         this.height = height;
+        this.side = side;
     }
     public double getArea()
     {
-        return base * height/2
+        return (base * height)/2;
+    }
+    public double getPerimeter()
+    {
+        return side * 2 + base;
     }
 }
